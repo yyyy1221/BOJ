af11086091a007a0ed69d63207d400705341d534
Heeyeon <heeyeon991221@gmail.com>
2021-01-19 17:15:36 +0900

    add 2875

---
+++ 2875.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int N; //여학생
+    int M; //남학생
+    int K; //인턴쉽에 참가해야하는 인원
+    
+    cin>>N>>M>>K;
+
+    int team = 0; //팀 개수
+    while(1){
+        if(N+M < K){
+            team--;
+            break;
+        }
+        N-=2;
+        M-=1;
+        team++;
+    }
+    cout<<team;
+}
