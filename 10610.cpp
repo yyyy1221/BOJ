7cc0689e6a33848a9030035195775f3c6ac32911
Heeyeon <heeyeon991221@gmail.com>
2021-01-20 16:04:11 +0900

    add 10610

--- 10610.cpp
+++ 10610.cpp
@@ -6,12 +6,12 @@ bool desc(int a, int b){
     return a > b;
 }
 int main(){
-    char arr[10];
+    char arr[100000];
     char ch;
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < 100000; i++){
         cin.get(ch);
         if(ch == '\n'){
-            for(; i < 10; i++){
+            for(; i < 100000; i++){
                 arr[i] = '\0';
             }
             break;
@@ -21,14 +21,14 @@ int main(){
 
     int result = 0;
     int count = 0; //0의 개수
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < 100000; i++){
         if(arr[i] == '0'){
             count++;
         }
     }
     
     int sum = 0;
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < 100000; i++){
         if(arr[i] != '\0'){
             sum += arr[i] - '0';
         }
@@ -37,14 +37,14 @@ int main(){
     if(sum%3 != 0)
         count = 0;
     
-    sort(arr, arr+10, desc);
-    for(int i = 0; i < 10; i++){
+    sort(arr, arr+100000, desc);
+    for(int i = 0; i < 100000; i++){
         if(count == 0){
             cout<<-1;
             break;
         }
+        if(arr[i] == '\0')
+            break;
         cout<<arr[i];
     }
-
-
 }
