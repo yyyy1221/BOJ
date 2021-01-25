a7ceba773a09f1582df4206cc58b908ce45561d3
Heeyeon <heeyeon991221@gmail.com>
2021-01-25 22:07:49 +0900

    add 10989 - 수 정렬하기 3

---
+++ boj_code/10989.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N;
+    cin>>N;
+    int arr[10001] = {0,};
+    for(int i = 0; i < N; i++){
+        int tmp;
+        cin>>tmp;
+        arr[tmp]++;
+    }
+    for(int i = 1; i < 10001; i++){
+        for(int j = 0; j < arr[i]; j++){
+            cout<<i<<"\n";
+        }
+    }
+    return 0;
+}
