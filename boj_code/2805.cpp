65d6f72c65f2f2a0fac7a00d608d43b5bf6a8f2b
Heeyeon <heeyeon991221@gmail.com>
2021-02-01 21:38:36 +0900

    2805번 - 나무 자르기

---
+++ boj_code/2805.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    long long N, M, result = 0;
+    vector<int> tree;
+    cin>>N>>M;
+    for(int i = 0; i < N; i++){
+        int x;
+        cin>>x;
+        tree.push_back(x);
+    }
+    long long start = 1, end = 2000000000;
+    while(start <= end){
+        long long mid = (start+end)/2;
+        long long sum = 0;
+        for(int i = 0; i < N; i++){
+            if(tree[i] > mid){
+                sum += tree[i] - mid;
+            }
+        }
+        if(sum >= M){
+            start = mid + 1;
+            if(result < mid){
+                result = mid;
+            }
+        }
+        if(sum < M){
+            end = mid - 1;
+        }
+    }
+    cout<<result;
+}
