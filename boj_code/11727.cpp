e801a0ab7ef225022ef76277629eafb452957ddb
Heeyeon <heeyeon991221@gmail.com>
2021-02-04 14:27:06 +0900

    11727번 2xn 타일링 2

---
+++ boj_code/11727.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    int dp[1001] = {0, };
+    cin>>n;
+    dp[1] = 1;
+    dp[2] = 3;
+    for(int i = 3; i <= n; i++){
+        dp[i] = (dp[i - 1] + 2*dp[i - 2])%10007;
+    }
+    cout<<dp[n];
+    return 0;
+}
