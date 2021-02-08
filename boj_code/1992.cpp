ce4318f249b04c50a0db58ba1390320b60c73ce0
yyyy1221 <heeyeon991221@gmail.com>
2021-02-08 21:33:36 +0900

    1992번 쿼드트리

---
+++ boj_code/1992.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int arr[64][64];
+bool same(int x, int y, int n){
+    for(int i = x; i < x + n; i++){
+        for(int j = y; j < y + n; j++){
+            if(arr[x][y] != arr[i][j]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+void solution(int x, int y, int n){
+    if(!same(x, y, n)){
+        cout<<"(";
+        int k = n/2;
+        for(int a = 0; a < 2; a++){
+            for(int b = 0 ; b < 2; b++){
+                solution(x+a*k, y+b*k, k);
+            }
+        }
+        cout<<")";
+        return;
+    }
+    //다르지 않으면
+    cout<<arr[x][y];
+}
+int main(){
+    int N;
+    cin>>N;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            scanf("%1d", &arr[i][j]);
+        }
+    }
+    solution(0, 0, N);
+    return 0;
+}
