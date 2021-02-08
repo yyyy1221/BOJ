7f8182755db325aa5d63c8d36c61dc92c95b31f8
yyyy1221 <heeyeon991221@gmail.com>
2021-02-08 21:35:32 +0900

    1992번 - 쿼드트리

--- boj_code/1992.cpp
+++ boj_code/1992.cpp
@@ -13,17 +13,17 @@ bool same(int x, int y, int n){
 }
 void solution(int x, int y, int n){
     if(!same(x, y, n)){
-        cout<<"(";
+        cout<<"("; //같지 않으면 분할 되니까 괄호 열기.
         int k = n/2;
         for(int a = 0; a < 2; a++){
             for(int b = 0 ; b < 2; b++){
                 solution(x+a*k, y+b*k, k);
             }
         }
-        cout<<")";
+        cout<<")"; //한구역 끝나면 괄호 닫기.
         return;
     }
-    //다르지 않으면
+    //다르지 않으면 출력.
     cout<<arr[x][y];
 }
 int main(){
