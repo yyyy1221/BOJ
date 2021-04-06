dcd6841a2f513161d35b5accb2296c7a04e6b43d
Heeyeon KIM <heeyeon991221@gmail.com>
2021-04-06 14:48:28 +0900

    4344번 - 평균은 넘겠지

--- boj_code/4344.cpp
+++ boj_code/4344.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main(){
-    
+	int C; //testcase 개수
+	float N; //student
+	float avg = 0; 
+	vector<float> answer;
+	cin>>C;
+	for(int i = 0; i < C; i++){
+			cin>>N;
+			vector<int> score;
+			score.clear();
+			int num = 0;
+			avg = 0;
+			for(int j = 0; j < N; j++){
+				cin>>num;
+				score.push_back(num);
+				avg += num;
+			}
+			avg /= N;
+			float cnt = 0;
+			for(int k = 0; k < N; k++){
+					if(score[k] > avg) cnt++;
+			}
+			answer.push_back((cnt/N)*100);
+	}
+	for(int i = 0; i < answer.size(); i++){
+			printf("%.3f%%\n", answer[i]);
+	}
+	return 0;
 }
