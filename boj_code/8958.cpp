c41cff3128f3316948cc41294276dcb95236f767
Heeyeon KIM <heeyeon991221@gmail.com>
2021-04-06 14:25:44 +0900

    8958번 ox퀴즈

--- boj_code/8958.cpp
+++ boj_code/8958.cpp
@@ -1,27 +1,35 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 int main(){
-    int testcase;
-    cin>>testcase;
+		int testcase;
+		cin >> testcase;
+		vector<int> score;
+		string ox;
+		int total_score = 0;
+		int now_score = 0;
+		for(int i = 0; i < testcase; i++){
+				cin>>ox;
+				for(int j = 0; j < ox.size(); j++){
+					if(ox.at(j) == 'O'){
+							now_score++;
+							total_score += now_score;
+					}
+					else if(ox.at(j) == 'X'){
+							now_score = 0;
+					}
+				}
+				score.push_back(total_score);
+				total_score = 0;
+				now_score = 0;
+		}
+		for(int i = 0; i < testcase; i++){
+				cout<<score[i]<<"\n";
+		}
 
-    string quiz;
-    int score[1000] = {0,};
-    
-    int plus = 1;
-    for(int i = 0; i < testcase; i++){
-        cin>>quiz;
-        for(int j = 0; j < quiz.size(); j++){
-            if(quiz.at(j) == 'O'){
-                score[i] += plus;
-                plus++;
-            }
-            else{
-                plus = 1;
-            }
-        }
-    }
-    for(int i = 0; i < testcase; i++){
-        cout<<score[i]<<endl;
-    }
-
+		return 0;
 }
+
+
+
