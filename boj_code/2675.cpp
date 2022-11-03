ea79ac66dd29a029e5b846f69031df52b5d22f4c
heeyeon <heeyeon991221@gmail.com>
2022-11-04 02:19:33 +0900

    2675번 문자열 반복

--- boj_code/2675.cpp
+++ boj_code/2675.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main(){
-    int T;
+    int T, R;
     cin>>T;
-    int R;
     string S;
     for(int i = 0; i < T; i++){
-        cin>>R;
-        cin.ignore();
-        cin>>S;
+        cin>>R>>S;
+        for(int i = 0; i < S.length(); i++){
+            for(int j = 0; j < R; j++){
+                cout<<S[i];
+            }
+        }
+        cout<<endl;
     }
 }
